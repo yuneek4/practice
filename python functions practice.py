30def6d11668b1962d12d8cb3da24f97c27df785
yuneek4 <yuneek4@gmail.com>
2017-03-02 15:01:25 -0500

    Add files via upload

---
+++ python functions practice.py
@@ -0,0 +1,19 @@
+'''
+def trianglearea(h,b):
+    area= (h*b)/2
+    return area
+print(trianglearea(3,8))
+'''
+'''
+def tempconvert(f):
+    cel= (f-32)/1.8
+    return cel
+temp1= tempconvert(32)
+temp2= tempconvert(95)
+print(temp1,temp2)
+'''
+
+def areasquare(sidelength):
+    area= sidelength**2
+    return area
+print(areasquare(5))
